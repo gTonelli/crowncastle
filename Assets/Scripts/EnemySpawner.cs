b4eedfa256c9fee41209a304bf43b7959e6cd71b
brandony <brandon.ypenburg@gmail.com>
2023-03-08 19:51:37 +0200

    Day night cycle and enemies spawning at night

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,19 +5,19 @@ using System.Threading.Tasks;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public int numEnemiesToSpawn;
+    public int numEnemiesToSpawn =10;
     public GameObject EnemyPrefab;
     private bool enemiesAreSpawning;
     private Vector3 lastEnemyPos; // #TODO prevent enemies from spawning on each other.
 
     private void OnEnable()
     {
-        GameState.OnChangeToNightTime += SpawnSkeletons;
+        TimeController.OnChangeToNightTime += SpawnSkeletons;
     }
 
     private void OnDisable()
     {
-        GameState.OnChangeToNightTime -= SpawnSkeletons;
+        TimeController.OnChangeToNightTime -= SpawnSkeletons;
     }
 
     // Start is called before the first frame update
@@ -28,20 +28,20 @@ public class EnemySpawner : MonoBehaviour
 
     private async void SpawnSkeletons()
     {
-        Instantiate(EnemyPrefab, transform.position + new Vector3(Random.Range(-3f, 3f), 0f, Random.Range(-1.5f, 1.5f)), new Quaternion(0f, -1f, 0f, 1f));
+        //Instantiate(EnemyPrefab, transform.position + new Vector3(Random.Range(-3f, 3f), 0f, Random.Range(-1.5f, 1.5f)), new Quaternion(0f, -1f, 0f, 1f));
 
-        // if (!enemiesAreSpawning)
-        // {
-        //     enemiesAreSpawning = true;
-        //     for (int _ = 0; _ < numEnemiesToSpawn; ++_)
-        //     {
-        //         await Task.Delay(2500 + Random.Range(0, 1250));
-        //         // #TODO
-        //         Instantiate(EnemyPrefab, transform.position + new Vector3(Random.Range(-3f, 3f), 0f, Random.Range(-1.5f, 1.5f)), new Quaternion(0f, -1f, 0f, 1f));
-        //     }
-        //     enemiesAreSpawning = false;
-        //     Debug.Log("Enemies Done spawning");
-        // }
+        if (!enemiesAreSpawning)
+        {
+            enemiesAreSpawning = true;
+            for (int _ = 0; _ < numEnemiesToSpawn; ++_)
+            {
+                await Task.Delay(2500 + Random.Range(0, 1250));
+                // #TODO
+                Instantiate(EnemyPrefab, transform.position + new Vector3(Random.Range(-3f, 3f), 0f, Random.Range(-1.5f, 1.5f)), new Quaternion(0f, -1f, 0f, 1f));
+            }
+            //enemiesAreSpawning = false;
+            Debug.Log("Enemies Done spawning");
+        }
 
     }
 }
