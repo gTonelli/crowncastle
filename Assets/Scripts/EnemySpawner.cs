41877f839b34034c19f16baf9dd62f71e568c32f
Nikolaos93 <jelic.nikolaa@gmail.com>
2023-03-11 19:51:58 +0100

    Resolved more conflicts.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,11 +5,7 @@ using System.Threading.Tasks;
 
 public class EnemySpawner : MonoBehaviour
 {
-<<<<<<< HEAD
-    public int numEnemiesToSpawn;
-=======
     public int numEnemiesToSpawn = 10;
->>>>>>> dev
     public GameObject EnemyPrefab;
     private bool enemiesAreSpawning;
     private Vector3 lastEnemyPos; // #TODO prevent enemies from spawning on each other.
@@ -32,22 +28,8 @@ public class EnemySpawner : MonoBehaviour
 
     private async void SpawnSkeletons()
     {
-        Instantiate(EnemyPrefab, transform.position + new Vector3(Random.Range(-3f, 3f), 0f, Random.Range(-1.5f, 1.5f)), new Quaternion(0f, -1f, 0f, 1f));
+        // Instantiate(EnemyPrefab, transform.position + new Vector3(Random.Range(-3f, 3f), 0f, Random.Range(-1.5f, 1.5f)), new Quaternion(0f, -1f, 0f, 1f));
 
-<<<<<<< HEAD
-        // if (!enemiesAreSpawning)
-        // {
-        //     enemiesAreSpawning = true;
-        //     for (int _ = 0; _ < numEnemiesToSpawn; ++_)
-        //     {
-        //         await Task.Delay(2500 + Random.Range(0, 1250));
-        //         // #TODO
-        //         Instantiate(EnemyPrefab, transform.position + new Vector3(Random.Range(-3f, 3f), 0f, Random.Range(-1.5f, 1.5f)), new Quaternion(0f, -1f, 0f, 1f));
-        //     }
-        //     enemiesAreSpawning = false;
-        //     Debug.Log("Enemies Done spawning");
-        // }
-=======
         if (!enemiesAreSpawning)
         {
             enemiesAreSpawning = true;
@@ -58,7 +40,6 @@ public class EnemySpawner : MonoBehaviour
                 Instantiate(EnemyPrefab, transform.position + new Vector3(Random.Range(-3f, 3f), 0f, Random.Range(-1.5f, 1.5f)), new Quaternion(0f, -1f, 0f, 1f));
             }
         }
->>>>>>> dev
 
     }
 }
