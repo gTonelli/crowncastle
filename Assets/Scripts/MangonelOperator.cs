844ea0c002aa9aa5fc3a55e98edd177363d25daf
Nikolaos93 <jelic.nikolaa@gmail.com>
2023-03-13 00:22:10 +0100

    More work on catapult prefabs and keep levels.

--- Assets/Scripts/MangonelOperator.cs
+++ Assets/Scripts/MangonelOperator.cs
@@ -6,7 +6,7 @@ public class MangonelOperator : MonoBehaviour
 {
 
     public Rigidbody weight;
-    public GameObject cannonball;
+    public GameObject cannonball, cannonball1, cannonball2, cannonball3, cannonball4, cannonball5;
 
     // Start is called before the first frame update
     void Start()
@@ -22,17 +22,27 @@ public class MangonelOperator : MonoBehaviour
             //release the weight
             weight.isKinematic = false;
 
-            //launch cannonball
-            /*FixedJoint fixedToDestroy;
+            //launch cannonballs
+            FixedJoint fixedToDestroy, fixedToDestroy1, fixedToDestroy2, fixedToDestroy3, fixedToDestroy4, fixedToDestroy5;
             fixedToDestroy = cannonball.GetComponent<FixedJoint>();
+            fixedToDestroy1 = cannonball1.GetComponent<FixedJoint>();
+            fixedToDestroy2 = cannonball2.GetComponent<FixedJoint>();
+            fixedToDestroy3 = cannonball3.GetComponent<FixedJoint>();
+            fixedToDestroy4 = cannonball4.GetComponent<FixedJoint>();
+            fixedToDestroy5 = cannonball5.GetComponent<FixedJoint>();
 
-            Destroy(fixedToDestroy);*/
+            Destroy(fixedToDestroy);
+            Destroy(fixedToDestroy1);
+            Destroy(fixedToDestroy2);
+            Destroy(fixedToDestroy3);
+            Destroy(fixedToDestroy4);
+            Destroy(fixedToDestroy5);
         }
 
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            //destroy projectiles
-            //Destroy(cannonball.gameObject);
-        }
+        // if (Input.GetKeyDown(KeyCode.Escape))
+        // {
+        //     //destroy projectiles
+        //     //Destroy(cannonball.gameObject);
+        // }
     }
 }
